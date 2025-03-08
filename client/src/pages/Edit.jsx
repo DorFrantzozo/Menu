5ba0f36d34894e3125785871bc5f14cf9a8ee69a
DorFrantzozo <dorfrant@gmail.com>
2025-03-08 13:53:58 +0200

    fix route problem5

--- client/src/pages/Edit.jsx
+++ client/src/pages/Edit.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
 import { useSelector } from "react-redux";
 const Edit = () => {
   const user = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+  if (!user) {
+    navigate("/");
+  }
+
   return (
     <div className="lg:h-[80vh]">
       <h2 className="text-2xl font-semibold  text-black sm:text-4xl flex justify-center mt-24">
