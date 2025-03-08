ca04e46abfa104223a099da09ded4bcce215b121
DorFrantzozo <dorfrant@gmail.com>
2025-03-08 13:58:13 +0200

    fix route problem7

--- client/src/pages/Edit.jsx
+++ client/src/pages/Edit.jsx
@@ -2,15 +2,28 @@ import { useNavigate } from "react-router-dom";
 import Card from "../components/Card";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+import Spinner from "@/components/Spinner";
 const Edit = () => {
   const user = useSelector((state) => state.user.user);
 
   const navigate = useNavigate();
+
+  // Run the useEffect only when user is updated, not on initial render
   useEffect(() => {
     if (!user) {
-      navigate("/");
+      navigate("/"); // Redirect to homepage if no user
     }
-  }, []);
+  }, [user, navigate]); // Added user as dependency to trigger useEffect when it changes
+
+  // Conditional rendering to avoid trying to access restaurantName if user is null
+  if (!user) {
+    return (
+      <div>
+        <Spinner />
+        ...
+      </div>
+    ); // Or a spinner/loading component
+  }
 
   return (
     <div className="lg:h-[80vh]">
