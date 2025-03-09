b1c6685ad22f2c23ef9e9a9fdd19b6496278925f
DorFrantzozo <dorfrant@gmail.com>
2025-03-09 14:09:20 +0200

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,7 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: /menu-seven-amber\.vercel\.app$/, // תומך בכל סאב-דומיין
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
