74b7d6aec7cc1f2a7a5fd462aa4ef202689ba275
DorFrantzozo <dorfrant@gmail.com>
2025-04-14 14:03:47 +0300

    working wifi settings

--- client/src/components/MenuSettings/MenuFeatures.jsx
+++ client/src/components/MenuSettings/MenuFeatures.jsx
@@ -1,28 +1,25 @@
 import { useState } from "react";
 import ShowWifi from "./ShowWifi";
 import { updateMenuSettings } from "@/utils/updateData";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "@/state/user/userSlice";
 
 const MenuFeatures = () => {
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
   const [wifiSettings, setWifiSettings] = useState({
     userId: user._id || null,
     wifiSsid: user.wifiSettings.ssid || "",
     wifiPassword: user.wifiSettings.wifiPassword || "",
-    displayWifi: user.wifiSettings.isEnabled || false,
+    displayWifi: user.wifiSettings.isEnabled,
   });
 
   const handleUpdateSettings = async () => {
     console.log(wifiSettings);
 
     try {
-      console.log(
-        wifiSettings.wifiSsid,
-        wifiSettings.wifiPassword,
-        user._id,
-        wifiSettings.displayWifi
-      );
-      await updateMenuSettings(wifiSettings);
+      const response = await updateMenuSettings(wifiSettings);
+      dispatch(updateUser(response.user));
     } catch (error) {
       console.error("Error updating menu settings:", error);
     }
