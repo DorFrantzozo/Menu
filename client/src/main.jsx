f4826027e0907374107597e494e4803aa60fefd1
DorFrantzozo <dorfrant@gmail.com>
2025-08-10 13:39:14 +0300

    bug fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./state/store.js";
-import "./i18n.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
